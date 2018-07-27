fba458c2a58500346695635e512101fc86af32bb
Derek Adams <derek.adams@sitewhere.com>
2018-07-26 22:16:59 -0400

    Fix font.

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
 
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -50,4 +50,3 @@ html {
   background-color: #35495e;
 }
 </style>
-
